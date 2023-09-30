bf332449727b30c19bc7fdd7838cabd324d3e1aa
Jonathan <jonathan.vigier@outlook.com>
2023-09-30 12:13:27 +0200

    Update: add a Home page as the homepage of the project

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavigationBar from "../../containers/NavigationBar";
+
+const Home = () => {
+  return (
+    <div className="homepage-container">
+      <NavigationBar />
+    </div>
+  );
+};
+
+export default Home;
