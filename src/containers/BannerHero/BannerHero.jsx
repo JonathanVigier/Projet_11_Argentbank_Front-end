18a6dee378d8b3da750d3533e08057acc714664a
Jonathan <jonathan.vigier@outlook.com>
2023-10-18 13:39:01 +0200

    Add: add a banner component + it's image and style file

---
+++ src/containers/BannerHero/BannerHero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BannerHero = () => {
+  return (
+    <div className="hero">
+      <section className="hero-content">
+        <p className="subtitle">No fees.</p>
+        <p className="subtitle">No minimum deposit.</p>
+        <p className="subtitle">High interest rates.</p>
+        <p className="text">Open a savings account with Argent Bank today!</p>
+      </section>
+    </div>
+  );
+};
+
+export default BannerHero;
