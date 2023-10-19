93487bc15c58dd78afe5ff8e906ae1caee57c9bc
Jonathan <jonathan.vigier@outlook.com>
2023-10-19 16:43:54 +0200

    Update: add a redux structure to the project with a Prrovider and a store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./router/Router";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router />
+    </React.StrictMode>
+  </Provider>
 );
