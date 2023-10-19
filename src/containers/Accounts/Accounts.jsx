420ad853bfb2d26742f7eb0aefd6bf31e98b4d8b
Jonathan <jonathan.vigier@outlook.com>
2023-10-19 16:35:22 +0200

    Add: add a component that includes all user's bank accounts

---
+++ src/containers/Accounts/Accounts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Account from "../../components/Account/Account";
+
+const Accounts = () => {
+  return (
+    <div>
+      <Account n={"x8349"} amount={"2,082.79"} />
+      <Account n={"x6712"} amount={"10,928.42"} />
+      <Account n={"x8349"} amount={"184.3"} />
+    </div>
+  );
+};
+
+export default Accounts;
