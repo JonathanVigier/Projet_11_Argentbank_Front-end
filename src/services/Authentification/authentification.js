10d19615d87a227839b00343eb699dc88d9ca1b9
Jonathan <jonathan.vigier@outlook.com>
2023-11-12 15:41:43 +0100

    Add: add the try/catch logic + import the serverErrorHandler function

--- src/services/Authentification/authentification.js
+++ src/services/Authentification/authentification.js
@@ -1,13 +1,21 @@
+import { serverErrorHandler } from "../../utils/ErrorHandler/error_handler";
+
 export const authUser = async (userCredentials) => {
-  const request = await fetch("http://localhost:3001/api/v1/user/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-    body: JSON.stringify(userCredentials),
-  });
+  try {
+    const request = await fetch("http://localhost:3001/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(userCredentials),
+    });
 
-  const response = await request.json();
+    const response = await request.json();
 
-  return response.body.token;
+    serverErrorHandler(response);
+    sessionStorage.setItem("user_tkn", response.body.token);
+    return response.body.token;
+  } catch (error) {
+    console.error(error);
+  }
 };
