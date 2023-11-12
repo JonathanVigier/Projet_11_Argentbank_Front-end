4a7c77054da027be15745c2071414083ba8e587b
Jonathan <jonathan.vigier@outlook.com>
2023-11-12 15:11:56 +0100

    Update: update local variables and the sign out function

--- src/components/SignInButton/SignInButton.jsx
+++ src/components/SignInButton/SignInButton.jsx
@@ -1,29 +1,36 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { resetToken } from "../../redux/Slicers/authSlice";
 import { resetUser } from "../../redux/Slicers/userSlice";
+import toast from "react-hot-toast";
 
 const Button = () => {
   const dispatch = useDispatch();
 
-  const isLogged = useSelector((state) => state.auth.token);
-  const userName = useSelector((state) => state.user.user.firstName);
+  const isUserLogged = sessionStorage.getItem("user_tkn");
+
+  const userName = sessionStorage
+    ? sessionStorage.getItem("user_firstName")
+    : null;
 
   const handleSignOut = () => {
+    localStorage.clear();
+    sessionStorage.clear();
     dispatch(resetToken());
     dispatch(resetUser());
+    toast.success("Successfully logged out");
   };
 
   return (
     <div>
-      {!isLogged ? (
+      {!isUserLogged ? (
         <NavLink to="/login" className="main-nav-item">
           <i className="fa fa-user-circle"></i> Sign In
         </NavLink>
       ) : (
         <div>
-          <NavLink to="/user" className="main-nav-item">
+          <NavLink to="/profile" className="main-nav-item">
             <i className="fa fa-user-circle"></i>
             {" " + userName}
           </NavLink>
