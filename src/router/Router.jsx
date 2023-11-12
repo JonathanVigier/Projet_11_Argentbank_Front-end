77e47184b4a9d0a0a89bd6ff0814974e57ffb3a5
Jonathan <jonathan.vigier@outlook.com>
2023-11-12 15:36:23 +0100

    Add: add a Toaster component to handle notifications + add a private route  component to access the profile page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,16 +4,48 @@ import Home from "../pages/Home/Home";
 import SignIn from "../pages/SignIn/SignIn";
 import User from "../pages/User/User";
 import ErrorPage from "../pages/Error/ErrorPage";
+import PrivateRoute from "../components/PrivateRoute/PrivateRoute";
+import Footer from "../containers/Footer/Footer";
+import { Toaster } from "react-hot-toast";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          error: {
+            style: {
+              border: "2px solid black",
+              padding: "12px",
+              background: "#FF9592",
+              color: "#FEFEFE",
+            },
+          },
+          success: {
+            style: {
+              border: "2px solid black",
+              padding: "12px",
+              background: "#0BD8B6",
+              color: "#FEFEFE",
+            },
+          },
+        }}
+      />
       <Routes>
-        <Route path="/profile" element={<User />} />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <User />
+            </PrivateRoute>
+          }
+        />
         <Route path="/login" element={<SignIn />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
