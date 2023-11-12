a0b9a935d71debe35d61dce03634195865c0c799
Jonathan <jonathan.vigier@outlook.com>
2023-11-12 15:44:00 +0100

    Add: add the try/catch logic + add the serverErrorHandler function

--- src/services/UserData/userData.js
+++ src/services/UserData/userData.js
@@ -1,28 +1,42 @@
+import { serverErrorHandler } from "../../utils/ErrorHandler/error_handler";
+
 export const fetchUserProfile = async (token) => {
-  const request = await fetch("http://localhost:3001/api/v1/user/profile", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  try {
+    const request = await fetch("http://localhost:3001/api/v1/user/profile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  const response = await request.json();
+    const response = await request.json();
 
-  return response.body;
+    serverErrorHandler(response);
+    sessionStorage.setItem("user_firstName", response.body.firstName);
+    sessionStorage.setItem("user_lastName", response.body.lastName);
+
+    return response.body;
+  } catch (err) {
+    console.error(err.status);
+  }
 };
 
-export const editUserProfile = async (token, editedUsername) => {
-  const request = await fetch("http://localhost:3001/api/v1/user/profile", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(editedUsername),
-  });
+export const editUserProfile = async (data) => {
+  try {
+    const request = await fetch("http://localhost:3001/api/v1/user/profile", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${data.token}`,
+      },
+      body: JSON.stringify(data),
+    });
 
-  const response = await request.json();
-
-  console.log(response.body);
+    const response = await request.json();
+    serverErrorHandler(response);
+    return response.body;
+  } catch (error) {
+    console.error(error);
+  }
 };
